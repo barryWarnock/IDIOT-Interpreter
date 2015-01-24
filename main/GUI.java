9ededa227963e3cb2229f22f9deff091c1237c52
frozensn0wfire <bolster@Jeremys-MacBook-Pro.local>
2015-01-23 17:52:54 -0800

    Stuff

--- main/GUI.java
+++ main/GUI.java
@@ -10,23 +10,7 @@ import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.Scanner;
 
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JEditorPane;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JSplitPane;
-import javax.swing.JTabbedPane;
-import javax.swing.JTextArea;
-import javax.swing.JToolBar;
-import javax.swing.KeyStroke;
+import javax.swing.*;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 
@@ -60,11 +44,7 @@ public class GUI implements ActionListener
         frame.setLocationRelativeTo(null);
 
         //Open a tab with a blank window
-        JComponent blankPanel=null;
-		try {
-			//The URL is fake but it can't be null otherwise the JEditPane is uneditable
-			blankPanel = makeNewTab(new File(""), true);
-		} catch (BadLocationException | FileNotFoundException ex) {}
+        JComponent blankPanel = makeNewTab();
 		tabbedPane.addTab("New File", null, blankPanel);
 
         
@@ -119,24 +99,22 @@ public class GUI implements ActionListener
 	/**
 	 * TODO this should have a close button on the edge of the tab
 	 * @param file a file that you would like to open in a JEditorPane
-	 * @param newTab If making a new tab set this to true 
 	 * @return JPanel with a JEditorPane in a JScrollPane
-	 * @throws BadLocationException 
+	 * @throws BadLocationException, FileNotFoundException
 	 */
-	public JComponent makeNewTab(File file, boolean newTab) throws BadLocationException, FileNotFoundException {
+	public JComponent openNewTab(File file) throws BadLocationException, FileNotFoundException {
 		
 		JPanel panel = new JPanel(false);
 		//Create a scrolled text area to type into
 		JEditorPane IDIOT_file_content = new JEditorPane();
 			
-		if(!newTab){
 			// scans file into the JEditPane
 			Scanner scan = new Scanner(file);
 			while (scan.hasNextLine()) {
 				String line = scan.nextLine();
 				Document doc = IDIOT_file_content.getDocument();
 				doc.insertString(doc.getLength(), line, null);	
-			}
+			
 			scan.close();
 		}
 		IDIOT_file_content.setEditable(true);
@@ -147,6 +125,26 @@ public class GUI implements ActionListener
 		panel.add(scroll);
 		return panel;	
 		}
+	
+	/**
+	 * TODO this should have a close button on the edge of the tab
+	 * @return JPanel with a JEditorPane in a JScrollPane
+	 */
+	public JComponent makeNewTab() 
+	{
+		
+		JPanel panel = new JPanel(false);
+		//Create a scrolled text area to type into
+		JEditorPane IDIOT_file_content = new JEditorPane();
+			
+		IDIOT_file_content.setEditable(true);
+		
+		JScrollPane scroll = new JScrollPane(IDIOT_file_content);
+		        
+		panel.setLayout(new BorderLayout());
+		panel.add(scroll);
+		return panel;	
+	}
 	
 	/**
 	 * 
@@ -333,92 +331,96 @@ public class GUI implements ActionListener
 	 
 	    // See what menuItem was clicked and do the appropriate thing 
 		//TODO find the appropriate thing to do, currently just prints a string
-		//TODO make this a switch to make it simpler 
-		if ("Copy".equals(action)) { 
-	      	System.out.println("Copy");
-	      	
-		} else if ("Cut".equals(action)) 
-		{ 
-			System.out.println("Cut");
+		switch(action){
+			case "Copy":{ 
+				System.out.println("Copy");
+				break;
+				
+			}case "Cut":{
+				System.out.println("Cut");
+				break;
+				
+			} case "Paste":{ 
+				System.out.println("Paste");
+				break;
+				
+			} case "New":{ 
+				
+				//Open a tab with a blank window
+				JComponent blankPanel= makeNewTab();
+				tabbedPane.addTab("New File", null, blankPanel);
+				break;
 			
-		} else if ("Paste".equals(action)) 
-		{ 
-			System.out.println("Paste");
+			} case "Open":{ 
+				System.out.println("Open");
+				break;
+				
+			} case "Print":{ 
+				System.out.println("Print");
+				break;
+				
+			} case "Save":{ 
+				System.out.println("Save");
+				break;
 			
-		} else if ("New".equals(action)) 
-		{ 
-			//Open a tab with a blank window
-			JComponent blankPanel=null;
-			try {
-				//The URL is fake but it can't be null otherwise the JEditPane is uneditable
-				blankPanel = makeNewTab(new File(""), true);
-			} catch (BadLocationException | FileNotFoundException ex) {}
-			tabbedPane.addTab("New File", null, blankPanel);
+			} case "Save As":{
+				System.out.println("Save As");
+				break;
 			
-		} else if ("Open".equals(action)) 
-		{ 
-			System.out.println("Open");
+			} case "Exit":{
+
+				//ask the user to save before closing 
+				Object[] options = {"Save and Exit", "Exit Without Saving","Cancel"};
+		
+				int result = JOptionPane.showOptionDialog(null, "Do you want to save before you close?","Close Dialog",
+				    JOptionPane.YES_NO_CANCEL_OPTION,  JOptionPane.PLAIN_MESSAGE, null, options, options[2]);
+				//find what the user clicked 
+				if (result == JOptionPane.YES_OPTION)
+				{
+					//TODO make this save the file then exit
+					//System.exit(0);
+				
+				} else if(result == JOptionPane.NO_OPTION)
+				{
+					System.exit(0);	
+				} 
+				
+				break;
+				
+			} case "Select All":{ 
+				System.out.println("Select All");
+				break;
 			
-		} else if ("Print".equals(action)) 
-		{ 
-			System.out.println("Print");
-			
-		} else if ("Save".equals(action)) 
-		{ 
-			System.out.println("Save");
-			
-		} else if ("Save As".equals(action)) 
-		{ 
-			System.out.println("Save As");
-			
-		} else if ("Exit".equals(action)) 
-		{ 
-			//ask the user to save before closing 
-			Object[] options = {"Save and Exit", "Exit Without Saving","Cancel"};
-	
-			int result = JOptionPane.showOptionDialog(null, "Do you want to save before you close?","Close Dialog",
-			    JOptionPane.YES_NO_CANCEL_OPTION,  JOptionPane.PLAIN_MESSAGE, null, options, options[2]);
-			//find what the user clicked 
-			if (result == JOptionPane.YES_OPTION)
-			{
-				//TODO make this save the file then exit
-				//System.exit(0);
-			
-			} else if(result == JOptionPane.NO_OPTION)
-			{
-				System.exit(0);	
-			} 
-		} else if ("Select All".equals(action)) 
-		{ 
-			System.out.println("Select All");
-			
-		} else if ("View Help".equals(action)) 
-		{ 
-			System.out.println("View Help");
-			
-		} else if ("About IDIOT IDE".equals(action)) 
-		{ 
-			//try to find a nice icon for the dialog box but don't display it if it can't be found
-			ImageIcon icon=null;
-			URL imageURL = this.getClass().getResource("/images/infoIcon.png");
-			if (imageURL != null) {                   
-				icon=new ImageIcon(imageURL,"Info");
-	        } 
-			
-			JOptionPane.showMessageDialog(null, "The IDIOT editor was made by:\n\n" 
-					+ "Jeremy Bolster,\n"
-					+ "Travis Kurucz,\n"
-					+ "Timothy Hillier,\n"
-					+ "Dean Kution,\n"
-					+ "Barry Warnock\n\n"
-					+ "Thanks to Visual Pharm for the icons which were released under\n"
-					+ "Creative Commons Attribution-No Derivative Works 3.0 Unported\n"
-					+ "They can be viewed at https://www.iconfinder.com/icons/27860/",
-					"About the IDIOT editor",JOptionPane.PLAIN_MESSAGE,icon);	
-			
-		} else if ("Compile".equals(action)) 
-		{ 
-			System.out.println("Compile");   
+			} case "View Help":{ 
+				System.out.println("View Help");
+				break;
+				
+			} case "About IDIOT IDE":{ 
+				
+				//try to find a nice icon for the dialog box but don't display it if it can't be found
+				ImageIcon icon=null;
+				URL imageURL = this.getClass().getResource("/images/infoIcon.png");
+				if (imageURL != null) {                   
+					icon=new ImageIcon(imageURL,"Info");
+		        } 
+				
+				JOptionPane.showMessageDialog(null, "The IDIOT editor was made by:\n\n" 
+						+ "Jeremy Bolster,\n"
+						+ "Travis Kurucz,\n"
+						+ "Timothy Hillier,\n"
+						+ "Dean Kution,\n"
+						+ "Barry Warnock\n\n"
+						+ "Thanks to Visual Pharm for the icons which were released under\n"
+						+ "Creative Commons Attribution-No Derivative Works 3.0 Unported\n"
+						+ "They can be viewed at https://www.iconfinder.com/icons/27860/",
+						"About the IDIOT editor",JOptionPane.PLAIN_MESSAGE,icon);
+				
+				break;
+				
+			} case "Compile":{
+				System.out.println("Compile");  
+				break;
+			}
 		}
 	}
 	
