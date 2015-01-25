94ec7d7d1dfe4d9c09db36dcb33c1b90d9cecf7d
frozensn0wfire <bolster@Jeremys-MacBook-Pro.local>
2015-01-24 21:08:07 -0800

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit ed282cf247124925a42ae740cef36dd86a52041e, reversing
    changes made to 367d7121d3321fe66c2690c8dd62866aefc0bb59.

--- main/GUI.java
+++ main/GUI.java
@@ -428,19 +428,8 @@ public class GUI implements ActionListener
 						//TODO make a popup error message
 						System.out.println("null :(");
 					}else{
-						
 						//sends a string to the interpreter
-<<<<<<< HEAD
 						interpreter.run(editor.getText());
-=======
-						//opens a new thread so infinite loops don't freeze the gui
-						Thread thread = new Thread() {
-					        public void run() {
-								//Interpreter.interpret(editor.getText());
-					        }
-					    };
-					    thread.start();
->>>>>>> origin/master
 					}
 				}
 				break;
