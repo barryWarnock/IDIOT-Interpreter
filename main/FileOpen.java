92f4a17d8c8b4f439517629708f698c332f29b7c
mildlywarm <Tim.r.hillier@gmail.com>
2015-01-27 16:41:09 -0800

    Fileopen with file save options.
    Trying to get it to save to the file named by the JPane. Only creates the file by the name, and does not write to it. Think this is because the name it gets is wrong.

--- main/FileOpen.java
+++ main/FileOpen.java
@@ -1,28 +1,81 @@
 package main;
 /** This opens the file through the file manager
- * 
+ * 1-26-15
  */
 
+import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
+import javax.swing.JScrollPane;
 
+import java.io.FileWriter;
 import java.io.File;
+import java.io.IOException;
+
 
 public class FileOpen {
-
+	//Stores the name of the file for use of the save function
+	static String fileName; 
+	static JEditorPane paneName= GUI.getPaneName();
+	
+	
+	
 	public static File fileManager() {
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-		int result = fileChooser.showOpenDialog(fileChooser);
+		int option = fileChooser.showOpenDialog(fileChooser);
 
 		// I can add a filter into this to get only a certain result
 
-		if (result == JFileChooser.APPROVE_OPTION) { 
-			// user selects file,return the selected file to whatever called this method
+		if (option == JFileChooser.APPROVE_OPTION) { 
+			// user selects file,return the selected file to whatever called this method			
 			File selectedFile = fileChooser.getSelectedFile();
+			fileName = (selectedFile.getName());
+			System.out.println(fileName); 
+			//paneName = fileName; 
 			return selectedFile;
 		} else {
 			return null;
 		}
+		 
 	}
+	
+	//This probably wont work. 
+	public static File fileSave() throws IOException  {
+		FileWriter text = new FileWriter(fileName);
+		System.out.println("file "+fileName); 
+		System.out.println("pane "+paneName); 
+		text.write(paneName.getText());
+		text.close();
+		return null; 
+		
+	}
+	//This has a better chance of working
+	//I think...
+	public static void fileSaveOmega(){
+		FileWriter out;
+		try {
+			out = new FileWriter(fileName);
+			//paneName is wrong i think
+			//fuck
+		
+			out.write(paneName.getText()); 
+			out.close(); 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	
+	}
+	
+	
+	}
+		
+	 
 
-}
+
+	
+	
+	
+	
+
+
