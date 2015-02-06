7375e7905f282c1adc1baa7d306dfe027713af7a
frozensn0wfire <bolster@Jeremys-MacBook-Pro.local>
2015-02-06 14:59:16 -0800

    Removed Unnecessary Code
    Removed duplicate code by using the new GUI.getFocusedEditor() method.

--- actions/CompileAction.java
+++ actions/CompileAction.java
@@ -5,45 +5,28 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.JEditorPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTabbedPane;
 
 @SuppressWarnings("serial")
 public class CompileAction extends AbstractAction {
 
-	private JTabbedPane tabbedPane = GUI.getTabbedPane();
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		//returns the tab component with focus
-		JScrollPane scroll = (JScrollPane) tabbedPane.getComponentAt(tabbedPane.getSelectedIndex());
-		if(scroll==null){
-			//TODO make a popup error message
-			System.out.println("null :(");
-		}else{
-			final JEditorPane editor = (JEditorPane) scroll.getComponent(0).getComponentAt(100, 100);
-			if(editor==null){
-				//TODO make a popup error message
-				System.out.println("null :(");
-			}else{
-				
-				Thread thread = new Thread() {
-			        public void run() {
-			        	//sends a string to the interpreter
-			        	try {
-							MainRun.getInterpreter().run(editor.getText());
-						} catch (Exception e1) {
-							//TODO error message pop-up
-							e1.printStackTrace();
-						}
-			        }
-			    };
-			    thread.start();
-				
-			    
+		JEditorPane editor = GUI.getFocusEditorPane();
+
+		Thread thread = new Thread() {
+			public void run() {
+				//sends a string to the interpreter
+				try {
+					MainRun.getInterpreter().run(editor.getText());
+				} catch (Exception e1) {
+					//TODO error message pop-up
+					e1.printStackTrace();
+				}
 			}
-		}
+		};
+		thread.start();
 	}
 
 }
--- actions/PrintAction.java
+++ actions/PrintAction.java
@@ -6,34 +6,20 @@ import java.awt.print.PrinterException;
 
 import javax.swing.AbstractAction;
 import javax.swing.JEditorPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTabbedPane;
 
 @SuppressWarnings("serial")
 public class PrintAction extends AbstractAction {
 
-	private JTabbedPane tabbedPane = GUI.getTabbedPane();
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		JScrollPane scroll = (JScrollPane) tabbedPane.getComponentAt(tabbedPane.getSelectedIndex());
-		if(scroll==null){
-			//TODO make a popup error message
-			System.out.println("null :(");
-		}else{
-			JEditorPane editor = (JEditorPane) scroll.getComponent(0).getComponentAt(100, 100);
-			if(editor==null){
-				//TODO make a popup error message
-				System.out.println("null :(");
-			}else{
-				try {
-					editor.print();
-				} catch (PrinterException e1) {
-					// TODO Auto-generated catch block
-				}
-			}
+		JEditorPane editor = GUI.getFocusEditorPane();
+		
+		try {
+			editor.print();	
+		} catch (PrinterException e1) {
+			// TODO Auto-generated catch block
 		}
 	}
-
 }
