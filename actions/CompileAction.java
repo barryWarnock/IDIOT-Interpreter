7260bf54af81812cf4821ad8b8bfc238d82d6ef8
JeremyBolster <bolster@Jeremys-MacBook-Pro.local>
2015-03-20 16:12:47 -0700

    Formatting

--- actions/CompileAction.java
+++ actions/CompileAction.java
@@ -4,23 +4,18 @@ import main.*;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
-import javax.swing.JTextPane;
 
 @SuppressWarnings("serial")
 public class CompileAction extends AbstractAction {
 
-	
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		
-
+	public void actionPerformed(ActionEvent e) 
+	{
 		Thread thread = new Thread() {
 			public void run() {
 				//sends a string to the interpreter
 				try {
-					JTextPane txt = GUI.getFocusTextPane();
-					MainRun.getInterpreter().run(txt.getText());
+					MainRun.getInterpreter().run(GUI.getFocusTextPane().getText());
 				} catch (Exception e1) {
 					//TODO error message pop-up
 					e1.printStackTrace();
@@ -29,5 +24,4 @@ public class CompileAction extends AbstractAction {
 		};
 		thread.start();
 	}
-
 }
